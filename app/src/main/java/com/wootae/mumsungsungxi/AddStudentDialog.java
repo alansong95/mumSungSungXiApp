8f0e018d95ae05d98529b5e5163f55d747fa213f
Alan Song <alansong95@gmail.com>
2018-08-07 15:37:29 +0900

    data validation and logic implemented in student add dialog fragment

--- app/src/main/java/com/wootae/mumsungsungxi/AddStudentDialog.java
+++ app/src/main/java/com/wootae/mumsungsungxi/AddStudentDialog.java
@@ -8,11 +8,14 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 /**
  * Created by Alan on 8/7/2018.
@@ -29,6 +32,7 @@ public class AddStudentDialog extends DialogFragment {
     private EditText etPhoneNumber;
     private EditText etEmail;
     private Spinner spinnerSection;
+    private Button btnConfirm;
 
     private String name;
     private String phoneNumber;
@@ -55,33 +59,75 @@ public class AddStudentDialog extends DialogFragment {
         etPhoneNumber = (EditText) view.findViewById(R.id.et_phone_number);
         etEmail = (EditText) view.findViewById(R.id.et_email);
         spinnerSection = (Spinner)  view.findViewById(R.id.spinner_section);
+        btnConfirm = view.findViewById(R.id.btn_confirm);
 
-        builder.setView(view);
-        builder.setTitle("Add Student");
-        builder.setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
+            public void onClick(View view) {
                 name = etName.getText().toString();
                 phoneNumber = etPhoneNumber.getText().toString();
                 email = etEmail.getText().toString();
 //                section = spinnerSection.getSelectedItem().toString();
                 section = "o";
 
-                String[] studentData = new String[4];
-                studentData[0] = name;
-                studentData[1] = phoneNumber;
-                studentData[2] = email;
-                studentData[3] = section;
+                if (TextUtils.isEmpty(name)) {
+                    Toast.makeText(getActivity().getApplicationContext(), R.string.enter_name, Toast.LENGTH_SHORT).show();
+                } else if (TextUtils.isEmpty(phoneNumber)) {
+                    Toast.makeText(getActivity().getApplicationContext(), R.string.enter_number, Toast.LENGTH_SHORT).show();
+                } else if (section.equals("선택")) {
+                    Toast.makeText(getActivity().getApplicationContext(), R.string.select_section, Toast.LENGTH_SHORT).show();
+                } else {
+                    String[] studentData = new String[4];
+                    studentData[0] = name;
+                    studentData[1] = phoneNumber;
+                    studentData[2] = email;
+                    studentData[3] = section;
 
-                mListener.onComplete(studentData);
-            }
-        }).setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-
+                    mListener.onComplete(studentData);
+                    dismiss();
+                }
             }
         });
+
+        builder.setView(view);
+        builder.setTitle(R.string.add_student);
+//        builder.setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialogInterface, int i) {
+//                name = etName.getText().toString();
+//                phoneNumber = etPhoneNumber.getText().toString();
+//                email = etEmail.getText().toString();
+////                section = spinnerSection.getSelectedItem().toString();
+//                section = "o";
+//
+//                if (TextUtils.isEmpty(name)) {
+//                    Toast.makeText(getActivity().getApplicationContext(), R.string.enter_name, Toast.LENGTH_SHORT).show();
+//                    return;
+//                } else if (TextUtils.isEmpty(phoneNumber)) {
+//                    Toast.makeText(getActivity().getApplicationContext(), R.string.enter_number, Toast.LENGTH_SHORT).show();
+//                    return;
+//                } else if (section.equals("선택")) {
+//                    Toast.makeText(getActivity().getApplicationContext(), R.string.select_section, Toast.LENGTH_SHORT).show();
+//                    return;
+//                }
+//
+//                String[] studentData = new String[4];
+//                studentData[0] = name;
+//                studentData[1] = phoneNumber;
+//                studentData[2] = email;
+//                studentData[3] = section;
+//
+//                mListener.onComplete(studentData);
+//            }
+//        }).setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialogInterface, int i) {
+//
+//            }
+//        });
 
         return builder.create();
     }
 }
+
+
